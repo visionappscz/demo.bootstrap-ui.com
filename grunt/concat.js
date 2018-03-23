c7f5887392dde3efe4ef45c8a9df835a0b9a455d
Vojtech Milersky <vojtech@visionapps.cz>
2018-03-23 14:55:36 +0100

    Fix path to Bootstrap-UI js

--- grunt/concat.js
+++ grunt/concat.js
@@ -11,7 +11,7 @@ module.exports = {
       '<%= paths.npm %>/select2/select2.js',
       '<%= paths.npm %>/select2/select2_locale_cs.js',
       '<%= paths.npm %>/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js',
-      '<%= paths.npm %>/bootstrap.ui/dist/js/bootstrap-ui.js',
+      '<%= paths.npm %>/bootstrap-ui/dist/js/bootstrap-ui.js',
       '<%= paths.npm %>/picturefill/dist/picturefill.js',
       '<%= paths.src %>/js/main.js',
     ],
